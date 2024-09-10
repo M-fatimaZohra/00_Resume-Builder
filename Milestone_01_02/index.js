0a84012780226b48681500652f4ba2a04fd7b679
M-fatimaZohra <fatima.zohra10122007@gmail.com>
2024-09-10 23:14:38 +0500

    firstMilestone

---
+++ Milestone_01_02/index.js
@@ -0,0 +1,25 @@
+"use strict";
+function showSkills() {
+    const skills = ["HTML", "CSS", "JS", "TS", "GITHUB", "NPM"];
+    let main = document.getElementById("Skills");
+    if (main) {
+        if (main.style.display == "none" || main.style.display == "") {
+            let css = main.style;
+            css.color = "#ffffff";
+            css.backgroundColor = "#295F98";
+            css.border = "1px solid #EAE4DD";
+            css.display = "block";
+            main.innerHTML = "";
+            skills.forEach((skill) => {
+                let element = document.createElement("li");
+                let main = document.getElementById("Skills");
+                let text = document.createTextNode(skill);
+                element.appendChild(text);
+                main?.appendChild(element);
+            });
+        }
+        else {
+            main.style.display = "none";
+        }
+    }
+}
